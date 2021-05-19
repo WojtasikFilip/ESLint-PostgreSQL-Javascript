6894cf15d95d83a2a77eb11af29287ecb2943bff
Filip <wojtasik.f03@htlwienwest.at>
2021-05-19 19:14:17 +0200

    Aufgabe 5

--- model/cocktails.js
+++ model/cocktails.js
@@ -8,6 +8,22 @@ async function getCocktails() {
   };
 }
 
+async function getCocktailZutat(cName) {
+  try {
+    const { rows } = await db.query(
+      'select zbez from cocktail join besteht b on cocktail.cid = b.cid join zutat z on b.zid = z.zid where cname = $1;',
+      [cName],
+    );
+    return {
+      code: 200,
+      data: rows,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 module.exports = {
   getCocktails,
+  getCocktailZutat,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const { getCocktails } = require('../model/cocktails');
+const { getCocktails, getCocktailZutat } = require('../model/cocktails');
 
 router.get(
   '/cocktails',
   asyncHandler(async (req, res) => {
     const result = await getCocktails();
     res.status(result.code).json(result);
-  })
+  }),
+);
+
+router.get(
+  '/cocktails/:name/zutaten',
+  asyncHandler(async (req, res) => {
+    const result = await getCocktailZutat(req.params.name);
+    res.status(result.code).json(result);
+  }),
 );
 
 module.exports = router;
