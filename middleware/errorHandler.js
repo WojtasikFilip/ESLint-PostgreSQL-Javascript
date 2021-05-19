22eaf7089ba07510f9aa6746281a5b3dc0e5b066
Filip <wojtasik.f03@htlwienwest.at>
2021-05-19 19:22:49 +0200

    Error Handler done

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,17 @@
 const errorHandler = (err, req, res, next) => {
+    console.log(`Error => ${err.message}`.red);
+    res.status(500).json({
+        code: 500,
+        data: 'Server error'
+    })
 };
 
 const notFound = (req, res) => {
+    console.log(`Not Found => ${err.message}`.red);
+    res.status(404).json({
+      code: 404,
+      data: 'Server error',
+    });
 };
 
 module.exports = { errorHandler, notFound };
